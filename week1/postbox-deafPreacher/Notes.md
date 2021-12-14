e2521accb4e9813a7b5acf1effbbb08e35a93e1a
shivam damre <shivamdamre913@gmail.com>
2021-12-14 14:38:32 +0530

    pushing week 1 progress

---
+++ week1/postbox-deafPreacher/Notes.md
@@ -0,0 +1,6 @@
+# Notes
+these are collection of small notes regarding the building and styling of this project.
+
+## Note 1
+starting a fresh create-react-app.
+Removed unnecessarry files.
---
+++ week1/postbox-deafPreacher/src/App.js
@@ -0,0 +1,13 @@
+import './App.scss';
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="header">        
+        <h1>Hello, World!</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
